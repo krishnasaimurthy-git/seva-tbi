cf4a44969cf42c8b800401fbae17ae49d25b47c5
Pammi Sai Krishna <krishnasaimurthy@gmail.com>
2026-03-23 17:14:44 +0530

    SEVA 2026 live

--- src/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://yueofaqggcexhkggsjrm.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1ZW9mYXFnZ2NleGhrZ2dzanJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NjM0NzYsImV4cCI6MjA4OTEzOTQ3Nn0.uO5CklzIClGS6P6p5XxNEOE2YkrD28vwRH93Aeue64o'
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
